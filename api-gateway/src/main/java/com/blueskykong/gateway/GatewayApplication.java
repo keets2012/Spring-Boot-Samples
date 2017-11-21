6425f6b4194e4efda9a7e063aaaff5e89b81507b
keets2012 <928420597@qq.com>
2017-11-21 10:19:24 +0800

    add config-client

--- api-gateway/src/main/java/com/blueskykong/gateway/GatewayApplication.java
+++ api-gateway/src/main/java/com/blueskykong/gateway/GatewayApplication.java
@@ -2,6 +2,7 @@ package com.blueskykong.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableZuulProxy
 public class GatewayApplication {
     public static void main(String[] args) {
--- config-client/src/main/java/com/blueskykong/config/client/ConfigClientApplication.java
+++ config-client/src/main/java/com/blueskykong/config/client/ConfigClientApplication.java
@@ -3,6 +3,8 @@ package com.blueskykong.config.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author keets
@@ -15,4 +17,5 @@ public class ConfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
+
 }
