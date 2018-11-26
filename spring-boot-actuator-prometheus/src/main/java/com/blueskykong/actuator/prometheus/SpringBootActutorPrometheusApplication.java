255ed5ac6c7c53a64f77f382c8fc6e36237d82ea
keets2012 <928420597@qq.com>
2018-11-26 21:24:15 +0800

    dev/add spring-boot-actuator-prometheus module

---
+++ spring-boot-actuator-prometheus/src/main/java/com/blueskykong/actuator/prometheus/SpringBootActutorPrometheusApplication.java
@@ -0,0 +1,12 @@
+package com.blueskykong.actuator.prometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActutorPrometheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActutorPrometheusApplication.class, args);
+	}
+}
---
+++ spring-boot-actuator-prometheus/src/test/java/com/blueskykong/actuator/prometheus/SpringBootActutorPrometheusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blueskykong.actuator.prometheus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootActutorPrometheusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
